331abd456369b28993da8a49ba72c746d557cbba
Ivan Nikolaenko <ivan.nikolaenko@unikie.com>
2023-07-26 15:21:49 +0300

    APP VM concept. Please read WARNING in generic-x86_64.nix
    Signed-off-by: Ivan Nikolaenko <ivan.nikolaenko@unikie.com>

--- targets/generic-x86_64.nix
+++ targets/generic-x86_64.nix
@@ -47,6 +47,7 @@
         ];
       }
     ];
+    appvmExtraModules = [{}];
     hostConfiguration = lib.nixosSystem {
       inherit system;
       specialArgs = {inherit lib;};
@@ -57,6 +58,7 @@
           ../modules/virtualization/microvm/microvm-host.nix
           ../modules/virtualization/microvm/netvm.nix
           ../modules/virtualization/microvm/guivm.nix
+          ../modules/virtualization/microvm/appvm.nix
           {
             ghaf = {
               hardware.x86_64.common.enable = true;
@@ -71,6 +73,10 @@
                 guivm = {
                   enable = true;
                   extraModules = guivmExtraModules;
+                };
+                appvm = {
+                  enable = true;
+                  extraModules = appvmExtraModules;
                 };
               };
 
